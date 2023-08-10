4f8926de8fa3ba5b17f1888a28fe2ee1d8bf4bad
Jader Correa <jadercorrea.hv@gmail.com>
2023-08-10 17:35:11 -0300

    Add asdf installation and github copilot plugin

--- new_mac_setup/apps.sh
+++ new_mac_setup/apps.sh
@@ -1,5 +1,5 @@
 # APPS
-apps=(
+gui_apps=(
   alfred
   bartender
   bettertouchtool
@@ -10,13 +10,24 @@ apps=(
   datagrip
   warp
   iterm2
-  tmux
-  tree
 )
 
 # Install apps to /Users/$user/Applications
-echo "installing apps..."
-brew install --appdir="/Applications" ${apps[@]}
+echo "installing gui apps..."
+brew install --appdir="/Applications" ${gui_apps[@]}
+
+service_apps=(
+  coreutils
+  curl
+  asdf
+  tmux
+  tree
+  legit
+)
+
+# Install apps to /opt/homebrew/bin
+echo "installing service apps..."
+brew install ${service_apps[@]}
 
 brew cleanup
 
@@ -30,3 +41,11 @@ ln -s ~/.dotfiles/vim/nvimrc ~/.config/nvim/init.vim
 
 mkdir -p ~/.vim
 ln -s ~/.dotfiles/vim/vim.plugins ~/.vim.plugins
+
+# Legit - will promp for [y/N]
+legit --install
+
+# LANGUAGE INSTALLATIONS
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf install nodejs latest
+asdf global nodejs latest
